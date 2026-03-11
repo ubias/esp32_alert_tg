fac5e67746e0819a9faaa1fad19e34d4daad2282
ubias <64230562+ubias@users.noreply.github.com>
2026-03-11 17:37:20 +0300

    Create secrets.py
    add secrets

---
+++ secrets.py
@@ -0,0 +1,14 @@
+# secrets.py
+
+#WI_FI
+WIFI_SSID = "wifi_name"
+WIFI_PASSWORD = "12345678"
+#SERVERS_LIST
+servers = {
+    "VPS_1": "https://ya.ru/",
+    "VPS_2": "https://google.com/",
+    "VPS_3": "https://ozon.ru/"
+}
+#TG_CREDIALS
+TELEGRAM_TOKEN = "871231231208:AAF7I4AAAAAAAAAAAAAAAAAAAAAAE6i4"      # TG TOKEN From BotFather
+TELEGRAM_CHAT_ID = "1232164873264"      # UR ID From UserInfoBot
